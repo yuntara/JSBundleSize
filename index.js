7fdeb0369020eb384ac04fa3f5adc6a72fe89f1e
yuntara <yuntara@mail.yuntara.net>
2021-03-14 20:23:39 +0900

    fix remove timing

--- index.js
+++ index.js
@@ -124,13 +124,13 @@ async function run() {
 
     console.log(`build base`);
     await exec.exec(build_command);
-
-    const before = get_files();
     for (const file of Object.values(before)) {
       if (file.path) {
         fs.unlinkSync(file.path);
       }
     }
+    const before = get_files();
+
     await exec.exec(`git checkout ${head_ref}`);
     const keys = Array.from(
       new Set([...Object.keys(before), ...Object.keys(after)])
